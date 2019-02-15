b9b01366ef0a8502154a6f0e22023ab0f51adee8
christiansallen <christiansallen@gmail.com>
2019-02-15 09:39:03 -0700

    Server setup was successful

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const actionsRouter = require("./routers/actions-router");
+const projectsRouter = require("./routers/projects-router");
+
+const server = express();
+server.use(express.json());
+
+// server.use("/api/actions", actionsRouter);
+// server.use("/api/projects", projectsRouter);
+
+server.get("/", (req, res) => {
+  res.send("<h2>Sprint Challenge Node Express</h2>");
+});
+
+module.exports = server;
